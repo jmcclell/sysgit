e9f93f11360b560095d4e172efd759a9b103e16f
Jason McClellan <jason@jasonmcclellan.io>
2022-09-19 17:46:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,161 @@
+# sysgit
+
+sysgit helps you manage your user configuration files across multiple systems.
+
+
+# What is it?
+
+sysgit isn't actually a separate program in and of itself, it's just a method
+for configuring a bare git repository to manage a subset of files in your
+`$HOME` directory.
+
+The idea for this came from: https://www.atlassian.com/git/tutorials/dotfiles
+
+This repository contains an installation script which will set up things up. It
+expects to be passed the location of a git repository containing your
+configuration files and will set up the bare repository appropriately. It will
+also configure an alias `sysgit` which will allow you to conveniently work with
+this particular git repo from anywhere.
+
+# Quickstart
+
+## Installation
+
+`/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jmcclell/sysgit/HEAD/install.sh)"`
+
+### Installation Options
+
+To augment the behavior of the installation script, you can export several
+environment variables to be made available when the script executes.
+
+```sh
+# The URL to the git repository used as the canonical storage for your system
+# configuration files, i.e. your dotfiles repo
+export SYSGIT_CONFIG_REPO="..."
+# The name of the config repo branch to clone. [Default: master]
+export SYSGIT_CONFIG_REPO_BRANCH="..."
+# Set this to make the installation non-interactive for remote installs [Default: unset]
+export NONINTERACTIVE=1
+# The desired location of the bare repository clone of the configuration repository. [Default: $HOME/.sysgit]
+export SYSGIT_HOME="..."
+# The path to the location you wish sysgit to manage. [Default: $HOME]
+export SYSGIT_WORKSPACE="..."
+# The path to install the sysgit executable script. [Default: /usr/local/bin]
+export SYSGIT_EXECUTABLE_PATH="..."
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jmcclell/sysgit/HEAD/install.sh)"
+```
+
+## Usage
+
+Under the hood, sysgit is just git. It ties your `$HOME` directory (by default)
+to a specified git repo, managing only the files that you specify in the git
+repo and ignoring all other files unless specifically added by you using the
+usual git workflow.
+
+So, given a configuration repo with the following structure
+
+```
+Root
+├── .config
+│  └── nvim
+│     └── init.lua
+├── .zshrc
+```
+
+sysgit will ensure that your `$HOME` directory contains those files from the
+configured branch of the configuration repo at installation time.
+
+To pull the latest remote changes:
+
+`sysgit pull`
+
+To commit local changes to e.g. `$HOME/.zshrc`:
+
+```sh
+sysgit add $HOME/.zshrc
+sysgit commit -m "Updated ZSH configuration"
+```
+
+To make local commits available to be pulled by other systems synced to the
+same configuration repository:
+
+`sysgit push`
+
+As you can see, the workflow is just git. Nothing more. All your usual git workflows work exactly the same way as usual, except the repository is tucked away into a bare repository, your workspace is set to `$HOME` regardless of your current working directory, and any files that aren't explicitly added to git are ignored by all its subcommands including e.g.
+
+`sysgit status`
+
+# How does it work?
+
+sysgit takes advantage of several git features: bare repositories, the ability
+to set `--work-tree` at git command invocation time, and the ability to
+configure a local repository to ignore untracked files.
+
+The following demonstrates the basic setup, though this installation script's
+actual installation is a bit more complex.
+
+```sh
+# Clone a bare repository
+git clone --bare $GIT_REPO_LOCATION $HOME/.sysgit
+
+# Create an alias which invokes git with the necessary directory information
+alias sysgit='$(command -v git) --git-dir=$HOME/.sysgit/ --work-tree=$HOME'
+
+# Ensure that untracked files under $HOME are not shown when working with
+# sysgit commands. Only files contained in the configuration repository should be
+# shown.
+sysgit config --local status.showUntrackedFiles no
+```
+
+# Why this installation script?
+
+While the setup is simple enough, it's nice to have a reliable way of
+initializing the sysgit concept on any machine in a way that
+
+1. works for both Linux and macOS
+2. works with any configuration repository
+3. handles pre-existing configuration files sanely
+
+That last point needs some explanation. When you have an existing system, it's
+likely that one or more files in your configuration repo will already be
+present, e.g. `$HOME/.vimrc` may already exist. This will prevent the `git
+checkout` command from working, as it will infer the pre-existing file as
+"local changes."
+
+To fix that issue, this script will back up all colliding files into
+`$HOME/.config-backup.$UNIX_TIMESTAMP/`
+
+# bootstrap file
+
+Along with initializing sysgit, this installation script can also execute a
+bootstrap file named `.sysgit-bootstrap` located at the root of your
+configuration repository. This script will be executed with bash from the
+installation script and can be used for any bootstrapping tasks, e.g.
+installation of software.
+
+It is recommended that any bootstrap script you create contain internal logic
+to detect OS and to assume a non-interactive mode so that you may use it for
+automated machine bootstrapping, e.g. ensuring you always have your preferred
+configuration within development Docker images.
+
+It is also recommended to make your bootstrap script idempotent so that it can
+be safely be ran more than once. This helps if you ever want to re-install
+sysgit from scratch or need to sync changes to the bootstrap file from another
+machine.
+
+# configuration repo
+
+Your configuration repo should mirror your $HOME directory, containing only the
+files you wish to manage. An example configuration repository structure might
+look like:
+
+```
+Root
+├── .config
+│  └── nvim
+│     └── init.lua
+├── .zshrc
+├── .zprofile
+└── .sysgit-bootstrap
+```
---
+++ install.sh
@@ -0,0 +1,283 @@
+#!/bin/bash
+#
+# Installs sysgit
+#
+# Requirements:
+# - instance of Bash executable at /bin/bash
+# - instance of git in $PATH (discoverable with `command -v`)
+#
+# Should work on macOS and Linux
+
+#                       #
+# --- Setup Options --- #
+#                       #
+
+set -e # Fail on any non-zero exit status
+set -u # Fail for referencing non-declared vars
+set -o pipefail # Don't silently swallow errors in pipelines
+
+
+#                              #
+# --- Setup Error Handling --- #
+#                              #
+
+# Aborts with error message and exits with status 1
+abort() {
+  printf "%s\n" "$@" >&2
+  exit 1
+}
+
+#                            #
+# --- Initial Pre-Checks --- #
+#                            #
+
+# Fail fast with a concise message when not using bash
+# Single brackets are needed here for POSIX compatibility
+if [ -z "${BASH_VERSION:-}" ]
+then
+  abort "Bash is required to interpret this script."
+fi
+
+# Check if script is run in POSIX mode
+if [ -n "${POSIXLY_CORRECT+1}" ]
+then
+  abort 'Bash must not run in POSIX mode. Please unset POSIXLY_CORRECT and try again.'
+fi
+
+# Check OS compatibility
+OS="$(uname)"
+if [[ "${OS}" == "Linux" ]]; then
+  IS_LINUX=1
+elif [[ "${OS}" != "Darwin" ]]; then
+  abort "sysgit is only supported on macOS and Linux."
+fi
+
+# Check if git is installed
+if ! command -v git >/dev/null 2>&1; then
+    abort "git is required to install sysgit!"
+fi
+
+# Check if both `INTERACTIVE` and `NONINTERACTIVE` are set
+if [[ -n "${INTERACTIVE-}" && -n "${NONINTERACTIVE-}" ]]
+then
+  abort 'Both `$INTERACTIVE` and `$NONINTERACTIVE` are set. Please unset at least one variable and try again.'
+fi
+
+#                                  #
+# --- Define String Formatters --- #
+#                                  #
+
+if [[ -t 1 ]]
+then
+  tty_escape() { printf "\033[%sm" "$1"; }
+else
+  tty_escape() { :; }
+fi
+
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_green="$(tty_mkbold 32)"
+tty_yellow="$(tty_mkbold 33)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+shell_join() {
+  local arg
+  printf "%s" "$1"
+  shift
+  for arg in "$@"
+  do
+    printf " "
+    printf "%s" "${arg// /\ }"
+  done
+}
+
+chomp() {
+  printf "%s" "${1/"$'\n'"/}"
+}
+
+info() {
+  printf "${tty_blue}>>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+warn() {
+  printf "${tty_yellow}Warning${tty_reset}: %s\n" "$(chomp "$1")"
+}
+
+err() {
+  printf "${tty_red}Error${tty_reset}: %s\n" "$(chomp "$1")"
+}
+
+#                                               #
+# --- Define Additional Non-Critical Checks --- #
+#                                               #
+
+# Check if script is run non-interactively (e.g. CI)
+# If it is run non-interactively we should not prompt for passwords.
+# Always use single-quoted strings with `exp` expressions
+# shellcheck disable=SC2016
+if [[ -z "${NONINTERACTIVE-}" ]]; then
+  if [[ ! -t 0 ]]; then
+    if [[ -z "${INTERACTIVE-}" ]]; then
+      warn 'Running in non-interactive mode because `stdin` is not a TTY.'
+      NONINTERACTIVE=1
+    else
+      warn 'Running in interactive mode despite `stdin` not being a TTY because `$INTERACTIVE` is set.'
+    fi
+  fi
+else
+  info 'Running in non-interactive mode because `$NONINTERACTIVE` is set.'
+fi
+
+#                                     #
+# --- Define User Input Functions --- #
+#                                     #
+
+getc() {
+  local save_state
+  save_state="$(/bin/stty -g)"
+  /bin/stty raw -echo
+  IFS='' read -r -n 1 -d '' "$@"
+  /bin/stty "${save_state}"
+}
+
+prompt_user() {
+  if [[ -z ${NONINTERACTIVE-} ]]; then
+    local c
+    local prompt="${1}"
+    if [[ -z $prompt ]]; then
+      abort "prompt_user requires a prompt argument."
+    fi
+    echo "${tty_bold}${prompt} "
+    getc c
+    if [[ "${c}" != "y" && "${c}" != "Y" ]]; then
+      return 1
+    fi
+  fi
+}
+
+check_run_command() {
+  local promptMsg="${1}"
+  if [[ -z ${promptMsg} ]]; then
+      abort "check_run_command requires promptMsg argument"
+  fi
+  if [[ -z ${3} ]]; then
+    local promptMsgSub=""
+    local cmd="${2}"
+  else
+    local promptMsgSub="${2}"
+    local cmd="${3}"
+  fi
+  if [[ -z ${cmd} ]]; then
+      abort "check_run_command requires cmd argument"
+  fi
+
+  info "${promptMsg}"
+  info "${tty_reset}${promptMsgSub}"
+  info "Command to run:"
+  info "    ${tty_reset}${cmd}"
+  echo
+  if prompt_user "Run command now? [Y/n]:"; then
+    info "Running command..."
+    eval "${cmd}"
+    return $?
+  else
+    err "Installation cancelled. Exiting."
+    return 1
+  fi
+}
+
+#                               #
+# --- Start of Installation --- #
+#                               #
+
+# Figure out what repository we're using for configuration
+config_repo_url=${1:-${SYSGIT_CONFIG_REPO:-}}
+config_repo_branch=${2:-${SYSGIT_CONFIG_REPO_BRANCH:-"master"}}
+
+if [[ -z "${config_repo_url}" ]]; then
+  if [[ -z "${NONINTERACTIVE:-}" ]]; then
+      #config_repo_input=($(ask_user "Which configuration repository do you want to use?"))
+      read -a config_repo_input -p "Configuration repository URL: "
+      config_repo_url=${config_repo_input[0]:-${config_repo_url}}
+      config_repo_branch=${config_repo_input[1]:-${config_repo_branch}}
+      if [[ -z "${config_repo_url}" ]]; then
+          abort "No configuration repository specified"
+      fi
+  else
+      abort "No configuration repository specified"
+  fi
+fi
+
+# Check if sysgit already exists
+SYSGIT_HOME=${SYSGIT_HOME:-"${HOME}/.sysgit"}
+SYSGIT_WORKSPACE=${SYSGIT_WORKSPACE:-"${HOME}"}
+
+if [[ -a "${SYSGIT_HOME}" ]]; then
+    check_run_command "Pre-existing sysgit installation found" "Do you wish to move it to a safe place to continue?" "mv '${SYSGIT_HOME}' '${SYSGIT_HOME}.backup_$(date +%s)'"
+fi
+
+if [[ ! -d "${SYSGIT_WORKSPACE}" ]]; then
+    check_run_command "Workspace ${SYSGIT_WORKSPACE} does not exist." "Do you wish to create it?" "mkdir -p '${SYSGIT_WORKSPACE}'"
+fi
+
+# Clone repository
+check_run_command "Cloning configuration repository" "Do you want to clone branch ${config_repo_branch} of ${config_repo_url} to bare repo ${SYSGIT_HOME}?" "git clone --bare -b ${config_repo_branch} ${config_repo_url} ${SYSGIT_HOME}"
+
+# Create sysgit function for use within this script
+sysgit() {
+  $(command -v git) --git-dir="${SYSGIT_HOME}/" --work-tree="${SYSGIT_WORKSPACE}" $@
+}
+
+# Ensure the local repo is configured to ignore untracked files
+sysgit config --local status.showUntrackedFiles no
+
+set +e
+backup_file_list=$(sysgit checkout 2>&1 | grep "^\t" | sed -e "s/^\t//")
+set -e
+
+if [[ ! -z "${backup_file_list}" ]]; then
+    backup_dir="${SYSGIT_WORKSPACE}/sysgit_existing_config_backup.$(date +%s)"
+    backup_file_list_formatted="$(echo "${tty_reset}${backup_file_list}" | sed -e "s/^/\t- /")"
+    check_run_command "Workspace has conflicting config changes." $'Backup the conflicting files?\n'"${backup_file_list_formatted}" "
+    IFS=\$'\n'
+    mkdir \"$backup_dir\"
+    for fname in \$backup_file_list; do
+      mkdir -p \$(dirname \"$backup_dir/\$fname\")
+      mv \"${SYSGIT_WORKSPACE}/\$fname\" \"$backup_dir/\$fname\"
+    done"
+fi
+
+info "Copying new configuration files"
+# Actually do the checkout of config files into the workspace
+sysgit checkout
+
+# Install sysgit script to /usr/local/bin
+set +e
+read -r -d '' sysgit_script <<"EOF"
+#!/bin/bash
+\$(command -v git) --git-dir="${SYSGIT_HOME}/" --work-tree="${SYSGIT_WORKSPACE}" \$@
+EOF
+set -e
+
+sysgit_executable="${SYSGIT_EXECUTABLE_PATH:-"/usr/local/bin"}/sysgit"
+
+info "Installing sysgit executable"
+if [[ -f "${sysgit_executable}" ]]; then
+  check_run_command "Pre-existing sysgit executable found at ${sysgit_executable}" "Overwrite?" "echo \"${sysgit_script}\" > \"${sysgit_executable}\""
+else
+  check_run_command "No sysgit executable found" "Create?" "echo \"${sysgit_script}\" > \"${sysgit_executable}\""
+fi
+
+# Ensure the sysgit command is executable
+chmod +x "${sysgit_executable}"
+
+# Run bootstrap script if present
+bootstrap_file="${SYSGIT_WORKSPACE}/.sysgit-bootstrap"
+if [[ -f "${bootstrap_file}" ]]; then
+  check_run_command "Bootstrap file found at ${bootstrap_file}" "Execute script?" "/bin/bash -c \"\$(cat \"${bootstrap_file}\")\""
+fi
+
+info "Installation complete."
